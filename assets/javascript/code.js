8491d7a5022cf5c4094a97301722862a1bbc6cc7
Navin K Jammula <navin_kumarj@intuit.com>
2017-12-16 11:56:25 -0800

    added form for family member

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -8,19 +8,29 @@
  };
  firebase.initializeApp(config);
 
+ var db = firebase.database();
+ 
  var myFamilyInfo;
 
  myFamilyInfo = [
-     { 'link': '1a', 'firstname': 'fn_grandfather', 'lastname': 'ln_grandfather', 'dob': '1/1/1950', 'birthplace': '', 'hobbies': '', 'profilepic': '' },
-     { 'link': '1b', 'firstname': 'fn_grandmother', 'lastname': 'ln_grandmother', 'dob': '1/1/1955', 'birthplace': '', 'hobbies': '', 'profilepic': '' },
-     { 'link': '2a', 'firstname': 'fn_father', 'lastname': 'ln_father', 'dob': '', 'birthplace': '1/1/1960', 'hobbies': '', 'profilepic': '' },
-     { 'link': '2b', 'firstname': 'fn_mother', 'lastname': 'ln_mother', 'dob': '', 'birthplace': '1/1/1965', 'hobbies': '', 'profilepic': '' },
-     { 'link': '3a', 'firstname': 'fn_self', 'lastname': 'ln_self', 'dob': '', 'birthplace': '1/1/1980', 'hobbies': '', 'profilepic': '' },
-     { 'link': '3b', 'firstname': 'fn_spouse', 'lastname': 'ln_spouse', 'dob': '1/1/1985', 'birthplace': '', 'hobbies': '', 'profilepic': '' },
-     { 'link': '4a', 'firstname': 'fn_kid1', 'lastname': 'ln_kid1', 'dob': '1/1/2010', 'birthplace': '', 'hobbies': '', 'profilepic': '' },
-     { 'link': '4b', 'firstname': 'fn_kid2', 'lastname': 'ln_kid2', 'dob': '1/1/2015', 'birthplace': '', 'hobbies': '', 'profilepic': '' }
+     { 'link': '1a', 'firstname': 'fn_grandfather', 'lastname': 'ln_grandfather', 'dob': '1/1/1950', 'birthplace': '', 'hobbies': '',  'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':'' },
+     { 'link': '1b', 'firstname': 'fn_grandmother', 'lastname': 'ln_grandmother', 'dob': '1/1/1955', 'birthplace': '', 'hobbies': '', 'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':''  },
+     { 'link': '2a', 'firstname': 'fn_father', 'lastname': 'ln_father', 'dob': '', 'birthplace': '1/1/1960', 'hobbies': '', 'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':''  },
+     { 'link': '2b', 'firstname': 'fn_mother', 'lastname': 'ln_mother', 'dob': '', 'birthplace': '1/1/1965', 'hobbies': '', 'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':''  },
+     { 'link': '3a', 'firstname': 'fn_self', 'lastname': 'ln_self', 'dob': '', 'birthplace': '1/1/1980', 'hobbies': '', 'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':''  },
+     { 'link': '3b', 'firstname': 'fn_spouse', 'lastname': 'ln_spouse', 'dob': '1/1/1985', 'birthplace': '', 'hobbies': '', 'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':''  },
+     { 'link': '4a', 'firstname': 'fn_kid1', 'lastname': 'ln_kid1', 'dob': '1/1/2010', 'birthplace': '', 'hobbies': '', 'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':''  },
+     { 'link': '4b', 'firstname': 'fn_kid2', 'lastname': 'ln_kid2', 'dob': '1/1/2015', 'birthplace': '', 'hobbies': '', 'occupation':'', 'profilepic': '', 'favmovie':'', 'favquote':'', 'favsong':''  }
  ]
 
  myFamilyInfo.forEach(function(person) {
- 	console.log(person.link, person.firstname);
+     console.log(person.link, person.firstname);
+     
+       
+     db.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+    });
  })
